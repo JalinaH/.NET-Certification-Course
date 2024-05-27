f243ce8beedace4265b480c03c89e0f48583c706
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-27 19:22:28 +0530

    chore: Refactor student scores calculation to use arrays and foreach loop

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f209e104c50f4f8e0ba7dcaa065b497f701a759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93569e812ae91802a78f1840d4aa47605ad03c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
