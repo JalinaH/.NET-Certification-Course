de23b14602af43d836ee32af9bed6a3135e6094c
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-01 12:32:23 +0530

    chore: Refactor AssemblyInfo.cs files to update AssemblyInformationalVersionAttribute

--- Guided-project-branching-looping-CSharp-main/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5b475d35e63549a13e3992cf830b9ae7c4a48f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76a255ae68267099fb66f13abab064417b6f571")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
