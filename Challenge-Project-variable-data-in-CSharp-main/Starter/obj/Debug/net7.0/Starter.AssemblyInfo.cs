d42236df8391d1252cc704b85dbb4a6bd4da549d
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-02 15:02:02 +0530

    chore: Remove unnecessary project files and update AssemblyInfo.cs files

--- Challenge-Project-variable-data-in-CSharp-main/Starter/obj/Debug/net7.0/Starter.AssemblyInfo.cs
+++ Challenge-Project-variable-data-in-CSharp-main/Starter/obj/Debug/net7.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14fc457aa09bd6f7cf47afaf7e3b469cfeaaba21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc85c832b2013cbea086750764808cda82f8cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
