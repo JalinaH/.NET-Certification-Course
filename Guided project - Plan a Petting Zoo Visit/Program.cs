5d7ba1eb8d5447114b7db6582389adad9c89af3a
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-03 11:02:18 +0530

    chore: Update petting zoo animals list and remove commented code

--- Guided project - Plan a Petting Zoo Visit/Program.cs
+++ Guided project - Plan a Petting Zoo Visit/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
